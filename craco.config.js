ee73626ed8f1a645144380f049461c2905ef51f2
WuJXcoder <coderwujx@gmail.com>
2023-04-05 12:09:40 +0800

    配置less

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,12 @@
 const path = require('path')
+const CracoLessPlugin = require('craco-less')
 
 module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin
+    },
+  ],
   webpack: {
     alias: {
       "@": path.resolve(__dirname, "src"),
