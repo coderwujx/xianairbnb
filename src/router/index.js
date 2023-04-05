686bfaf248f81a21a19a2517ffb42a604a0b6b0b
WuJXcoder <coderwujx@gmail.com>
2023-04-05 13:26:08 +0800

    路由的配置

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Navigate } from "react-router-dom"
+
+const Home = React.lazy(() => import('../views/home/index'))
+const Detail = React.lazy(() => import('../views/detail/index'))
+const Entire = React.lazy(() => import('../views/entire/index'))
+
+const routes = [
+  {
+    path: '',
+    element: <Navigate to="/home" />
+  },
+  {
+    path: '/home',
+    element: <Home />
+  },
+  {
+    path: '/detail',
+    element: <Detail />
+  },
+  {
+    path: '/entire',
+    element: <Entire />
+  },
+]
+
+export default routes
