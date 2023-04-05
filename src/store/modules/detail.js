7f84c14f9a21989d1af554476618977f1497edb0
WuJXcoder <coderwujx@gmail.com>
2023-04-05 14:35:32 +0800

    配置redux

---
+++ src/store/modules/detail.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const detailSlice = createSlice({
+  name: "detail",
+  initialState: {
+
+  },
+  reducers: {
+
+  }
+})
+
+export default detailSlice.reducer
