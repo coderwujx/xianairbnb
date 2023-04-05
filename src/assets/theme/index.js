66e2da795994655568836e955ff447e669405b8f
WuJXcoder <coderwujx@gmail.com>
2023-04-05 21:46:03 +0800

    制定主题颜色

---
+++ src/assets/theme/index.js
@@ -0,0 +1,8 @@
+const theme = {
+  color: {
+    primaryColor: "#ff385c",
+    seconddaryColor: "#00848A"
+  }
+}
+
+export default theme
--- src/components/AppHeader/c-cpns/HeaderLeft/style.js
+++ src/components/AppHeader/c-cpns/HeaderLeft/style.js
@@ -2,4 +2,12 @@ import styled from "styled-components"
 
 export const LeftWrapper = styled.div`
   flex: 1;
+  display: flex;
+  color: ${props => props.theme.color.primaryColor};
+  align-items: center;
+
+  .logo{
+    margin-left: 24px;
+    cursor: pointer;
+  }
 `
