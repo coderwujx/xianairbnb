668e8ff21bc2ffb8fd01621cc7637c464c753c55
WuJXcoder <coderwujx@gmail.com>
2023-04-05 22:13:45 +0800

    headerRight布局样式

--- src/assets/theme/index.js
+++ src/assets/theme/index.js
@@ -2,6 +2,18 @@ const theme = {
   color: {
     primaryColor: "#ff385c",
     seconddaryColor: "#00848A"
+  },
+  textColor: {
+    primaryColor: "#484848",
+    seconddaryColor: "#222"
+  },
+  mixins: {
+    boxShadow: `
+      transition: box-shadow 200ms ease;
+      &:hover{
+        box-shadow: 0 2px 4px rgba(0, 0, 0, .18);
+      }
+    `
   }
 }
 
