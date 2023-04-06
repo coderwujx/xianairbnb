9ad562fdc750fc31dab41688eae2cfc6264abd92
WuJXcoder <coderwujx@gmail.com>
2023-04-06 21:32:14 +0800

    footer搭建

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,4 @@
 import WJXRequest from './request'
 
 export default WJXRequest
+export * from "./modules/home"
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,9 +1,30 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect } from 'react'
+import { HomeWrapper } from './style'
+import HomeBanner from './c-cpns/HomeBanner'
+import { fetchHomeDataAction } from '@/store/modules/home'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+import SectionHeader from '@/components/SectionHeader'
 
 const home = memo(() => {
+  const { goodPriceInfo } = useSelector((state) => ({
+    goodPriceInfo: state.home.goodPriceInfo
+  }), shallowEqual)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchHomeDataAction())
+  }, [dispatch])
 
   return (
-    <div>home</div>
+    <HomeWrapper>
+      <HomeBanner />
+      <div className="content">
+        <div className="good-top">
+          <SectionHeader title={goodPriceInfo.title} />
+        </div>
+      </div>
+    </HomeWrapper>
   )
 })
 
